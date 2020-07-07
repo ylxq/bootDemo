44611e68f8fc5124c6f6c5dede777057e503ffb3
taowenchen <taowenchen@gitlab.com>
2020-07-07 19:44:11 +0800

    班级页面

--- src/main/frontend/src/views/index.vue
+++ src/main/frontend/src/views/index.vue
@@ -1,52 +1,75 @@
-<style scoped lang="less">
-    .index{
-        width: 100%;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
+<style scoped>
+    .layout {
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+
+    .layout-logo {
+        width: 100px;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+    }
+
+    .layout-nav {
+        width: 420px;
+        margin: 0 auto;
+        margin-right: 20px;
+    }
+
+    .layout-footer-center {
         text-align: center;
-        h1{
-            height: 150px;
-            img{
-                height: 100%;
-            }
-        }
-        h2{
-            color: #666;
-            margin-bottom: 200px;
-            p{
-                margin: 0 0 50px;
-            }
-        }
-        .ivu-row-flex{
-            height: 100%;
-        }
     }
 </style>
 <template>
-    <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="">
-                </h1>
-                <h2>
-                    <p>Welcome to your View UI app!</p>
-                    <Button @click="handleStart">Start View UI</Button>
-                </h2>
-            </Col>
-        </Row>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="4" style="float: right">
+                            <Avatar style="background-color: #87d068" icon="ios-person"/>
+                            欢迎您！{{$store.getters.userInfo.name}}
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content :style="{padding: '0 50px'}">
+                <Card :style="{margin: '50px 0'}" style="min-height: 600px;">
+                    <p slot="title">
+                        <Icon type="ios-film-outline"></Icon>
+                        Classic film
+                    </p>
+                    <a href="#" slot="extra" @click.prevent="addClazz">
+                        <Icon type="ios-loop-strong"></Icon>
+                        Change
+                    </a>
+                    <div>
+                        Content
+                    </div>
+
+                </Card>
+            </Content>
+            <Footer class="layout-footer-center">2020-2016 &copy; TalkingData</Footer>
+        </Layout>
     </div>
 </template>
 <script>
     export default {
+        data() {
+            return {}
+        },
         methods: {
-            handleStart () {
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of View UI.'
-                });
+            addClazz() {
+
             }
         }
     }
